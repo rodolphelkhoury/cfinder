626d11b84334a3c361e36fdaadbbcfea56a3d0e7
Rodolph Khoury <rodolph@tomorrow.services>
2025-04-26 01:27:25 +0300

    better auth ui

--- composeApp/src/commonMain/kotlin/org/composempfirstapp/project/core/Constants.kt
+++ composeApp/src/commonMain/kotlin/org/composempfirstapp/project/core/Constants.kt
@@ -20,7 +20,7 @@ import org.composempfirstapp.project.core.navigation.MainRouteScreen
 import org.jetbrains.compose.resources.StringResource
 
 const val datastoreFileName = "setting.preferences_pb"
-const val BASE_URL = "https://mbyhbaxctq.sharedwithexpose.com/api/"
+const val BASE_URL = "https://hf1bu5ktev.sharedwithexpose.com/api/"
 
 enum class Theme(val title: StringResource) {
     SYSTEM_DEFAULT(Res.string.system_default),
