7fbf201ac5d6eb87c4329dcd675c6eed1349559d
perlamikhael <perlamikhael2003@gmail.com>
2025-04-27 10:53:30 +0300

    new design

--- composeApp/src/commonMain/kotlin/org/composempfirstapp/project/core/Constants.kt
+++ composeApp/src/commonMain/kotlin/org/composempfirstapp/project/core/Constants.kt
@@ -20,7 +20,7 @@ import org.composempfirstapp.project.core.navigation.MainRouteScreen
 import org.jetbrains.compose.resources.StringResource
 
 const val datastoreFileName = "setting.preferences_pb"
-const val BASE_URL = "https://dxvaccfedu.sharedwithexpose.com/api/"
+const val BASE_URL = "https://gobadoo0ll.sharedwithexpose.com/api/"
 
 enum class Theme(val title: StringResource) {
     SYSTEM_DEFAULT(Res.string.system_default),
